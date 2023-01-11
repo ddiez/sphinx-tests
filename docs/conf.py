653a92c8c222700f234cf3adda80804adae4a7ce
Diego Diez <diego10ruiz@gmail.com>
2023-01-11 17:19:40 +0900

    Add additional info to conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,14 @@ extensions = [
   "myst_parser"
 ]
 
-#import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 
 project="sphinx-tests"
+
+html_context = dict(
+    display_github=True,      # Integrate GitHub
+    github_user='ddiez',   # Username
+    github_repo='sphinx-tests',     # Repo name
+    github_version='main',  # Version
+    conf_py_path='/',    # Path in the checkout to the docs root
+)
